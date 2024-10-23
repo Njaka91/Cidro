a9700e45229a18c8b5b87252af9d453ceaee3010
Njaka91 <andriaravelo91@gmail.com>
2024-10-23 17:20:02 +0300

    recommit

--- src/components/TitleTexte.jsx
+++ src/components/TitleTexte.jsx
@@ -1,17 +1,19 @@
 // eslint-disable-next-line react/prop-types
-const TitleTexte = ({title, text}) => {
+const TitleTexte = ({ title, text }) => {
   return (
-    <div className="flex flex-col justify-center px-6 mt-12 text-[#1B2C51]
+    <div
+      className="flex flex-col justify-center px-6 mt-12 text-[#1B2C51]
     md:px-8 
       lg:px-24 
-      xl-px-40">
+      xl-px-40"
+    >
       <div className=" flex flex-col text-center items-center text-6xl font-extrabold ">
-        <span>{title}</span>
-        <div className="bg-fuchsia-900 w-1/2 h-1 rounded-full mt-3" />
+        <div className="flex flex-col items-center">
+          <span>{title}</span>
+          <div className="bg-fuchsia-900 w-1/2 h-1 rounded-full mt-3" />
+        </div>
       </div>
-      <span className="text-center mt-7">
-        {text}
-      </span>
+      <span className="text-center mt-7 mx-32">{text}</span>
     </div>
   );
 };

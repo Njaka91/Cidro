416143657b786b4fdc02b7d3ff1f7243f613e6fc
Njaka91 <andriaravelo91@gmail.com>
2024-10-25 08:46:41 +0300

    correction image stubbornimage

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -7,7 +7,10 @@ import AppointmentBottom from "./AppointmentBottom";
 import { useLocation } from "react-router-dom";
 import { useEffect } from "react";
 import bgimg from "../assets/images/photeuillRazakatiana - Copie.jpg";
-import bgimg2 from "../assets/images/cabinet_dentaire.jpg";
+import implant from "../assets/images/Nos soins/Implantologie/implant_dentaire.png";
+import orthodont from "../assets/images/Nos soins/Orthodontie/orthodontie_cidro.png";
+import chirurgieOrale from "../assets/images/Nos soins/Chirurgie dentaire/Chir_orale.png";
+import dentisterieGenerale from "../assets/images/Nos soins/Dentisterie générale/dentisterie_CIDRO.jpg";
 import Button from "./Button"
 
 
@@ -18,7 +21,16 @@ const Root = () => {
     window.scrollTo(0, 0);
   }, [pathname]);
 
-  const imgBg = pathname === "/" ? bgimg : bgimg2;
+  let imgBg = bgimg
+
+  switch (pathname) {
+    case "/" : imgBg = bgimg; break;
+    case "/implantologie" : imgBg = implant; break;
+    case "/orthodontie" : imgBg = orthodont; break;
+    case "/chirurgie-orale" : imgBg = chirurgieOrale; break;
+    case "/dentisterie-generale" : imgBg = dentisterieGenerale; break;
+    default : imgBg = bgimg; break;
+  }
 
   return (
     <>
